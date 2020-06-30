7408f2079d1db186a6cf1bb2f154dcf041e32c3d
mikhawa <michaeljpitz@gmail.com>
2020-06-30 16:25:59 +0200

    javascript.php created

--- pages/accueil.php
+++ pages/accueil.php
@@ -17,3 +17,21 @@
 
 </head>
 <body>
+<?php
+include "menu_deconnect.php";
+?>
+<header class="jumbotron">
+    <h1 class="display-4 text-center mb-4">Portfolio | Page d'accueil</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed itaque neque doloremque animi est praesentium. Autem dignissimos ipsam itaque quibusdam placeat iste accusamus quasi modi reiciendis. Cumque neque aut, excepturi.</p>
+</header>
+
+<!--
+ici notre contenu
+-->
+
+<?php
+require_once "javascript.php";
+?>
+
+</body>
+</html>
