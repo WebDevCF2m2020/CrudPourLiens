753ce4073dd19eb64aa30413faf67e162b08f5da
mikhawa <michaeljpitz@gmail.com>
2020-06-30 16:48:23 +0200

    form into formulaire.php ok

--- pages/formulaire.php
+++ pages/formulaire.php
@@ -25,9 +25,32 @@ include "menu_deconnect.php";
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed itaque neque doloremque animi est praesentium. Autem dignissimos ipsam itaque quibusdam placeat iste accusamus quasi modi reiciendis. Cumque neque aut, excepturi.</p>
 </header>
 
-<!--
-ici notre contenu
--->
+<main class="container">
+    <form method="post" action="">
+        <div class="form-group form-row">
+            <label for="votreEmail" class="col-3">Votre email</label>
+            <input type="email" class="form-control col-9" id="votreEmail" aria-describedby="emailHelp" placeholder="Entrez votre e-mail">
+        </div>
+        <div class="form-group form-row">
+            <label for="votreNom" class="col-3">Votre nom</label>
+            <input type="text" class="form-control col-9" id="votreNom" placeholder="Entrez votre nom" maxlength="45" size="45" >
+            <small class="form-text offset-3 d-none" id="msgErreur">Les données introduites dépassent la limite (max 45)</small>
+        </div>
+        <div class="form-group form-row">
+            <label for="votreSociete" class="col-3">Votre société</label>
+            <input type="text" class="form-control col-9" id="votreSociete" aria-describedby="emailHelp" placeholder="Entrez le nom de votre société" maxlength="45" size="45" >
+        </div>
+        <div class="form-group form-row">
+            <label for="sujetMessage" class="col-3">Le sujet de votre message</label>
+            <input type="text" class="form-control col-9" id="sujetMessage" aria-describedby="emailHelp" placeholder="Entrez le sujet de votre message">
+        </div>
+        <div class="form-group form-row">
+            <label for="votreMessage" class="col-3">Votre message</label>
+            <textarea class="form-control col-9" id="votreMessage" placeholder="Entrez votre message"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Envoyer</button>
+    </form>
+</main>
 
 <?php
 require_once "javascript.php";
