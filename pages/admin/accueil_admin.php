435cc2fee6100155009e469194120789a1dcaef6
mikhawa <michaeljpitz@gmail.com>
2020-07-03 10:44:02 +0200

    disconnect.php ok

--- pages/admin/accueil_admin.php
+++ pages/admin/accueil_admin.php
@@ -1,3 +1,7 @@
 <?php
-var_dump($_SESSION);
+// protection de l'accès à cette page, si la session n'existe pas OU qu'elle n'est pas ou plus valide
+if(!isset($_SESSION['notresession'])||$_SESSION['notresession']!==session_id()) {
+    // on efface définitivement la session et on est redirigé sur la page d'accueil publique
+    header("Location: disconnect.php");
+}
 ?>
